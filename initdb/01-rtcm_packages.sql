c85e0d76bb7f5e8c8dc4eca03a2c1977244d8dc3
Lars Stenseng <lars@stenseng.net>
2021-11-29 12:47:23 +0100

    Adding db init script

---
+++ initdb/01-rtcm_packages.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS rtcm_packages (
+  rtcm_package_id SERIAL,
+  receive_time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  mountpoint varchar(50),
+  rtcm_obs_epoch NUMERIC(10, 3),
+  rtcm_msg_type SMALLINT NOT NULL,
+  rtcm_msg_size INTEGER,
+  rtcm_sat_count SMALLINT
+);
+SELECT create_hypertable('rtcm_packages', 'receive_time', 'mountpoint', 2);
+CREATE INDEX ON rtcm_packages(‘mountpoint’, ‘rtcm_msg_type’, ‘receive_time’ DESC);
+CREATE INDEX ON rtcm_packages(‘mountpoint’, ‘rtcm_msg_type’, ‘receive_time’, ‘rtcm_obs_epoch’ DESC);
+CREATE INDEX ON rtcm_packages(‘rtcm_package_id’ DESC);
---
+++ initdb/02-gnss_observations.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS gnss_observations (
+  obs_id SERIAL,
+  rtcm_package_id  INTEGER,
+  obs_epoch TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  rtcm_msg_type SMALLINT NOT NULL,
+  mountpoint varchar(50),
+  sat_id char(4),
+  sat_signal char(3),
+  obs_code NUMERIC(10, 3),
+  obs_phase NUMERIC(10, 3),
+  obs_doppler NUMERIC(8, 4),
+  obs_snr NUMERIC(6, 4)
+);
+SELECT create_hypertable('gnss_observations', 'obs_time', 'mountpoint', 2);
+CREATE INDEX ON gnss_observations(‘mountpoint’, ‘sat_id’, ‘sat_signal’, ‘obs_epoch’ DESC);
+CREATE INDEX ON gnss_observations(‘mountpoint’, ‘rtcm_msg_type’, ‘obs_epoch’ DESC);
